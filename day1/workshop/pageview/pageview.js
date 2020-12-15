f54a59922668d90e30f41af490648d913e846de8
Marc Theunissen <mtheunissen82@gmail.com>
2020-12-15 20:32:26 +0100

    Add redis kv-store

--- day1/workshop/pageview/pageview.js
+++ day1/workshop/pageview/pageview.js
@@ -1,16 +1,28 @@
 const os = require('os');
 const http = require('http');
+const redis = require('redis');
 
 const port = 8080;
 
-let counter = 0;
+const redisHost = 'redis';
+const redisPort = 6379;
+
+const client = redis.createClient({host:redisHost, port: redisPort});
+
+client.on('error', function(error) {
+    console.error(error);
+});
+
+client.setnx('counter', 0);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Pageview: ${counter} (from hostname: ${os.hostname()})`);
 
-  counter++;
+  client.get('counter', function(err, result) {
+    res.end(`Pageview: ${result} (from hostname: ${os.hostname()})`);
+    client.incr('counter');
+  });
 });
 
 server.listen(port, () => {
